d25918a80b223c54d823da4fd8d4114376fac289
jjrv <juha@busfaster.com>
2018-11-02 00:45:48 +0200

    Improve build system.

--- rollup.config.js
+++ rollup.config.js
@@ -1,15 +1,1 @@
-const pkg = require('./package.json');
-
-module.exports = {
-	input: pkg.module,
-	output: [
-		{
-			file: pkg.main,
-			format: 'cjs'
-		}, {
-			file: pkg.browser,
-			name: pkg.name,
-			format: 'umd'
-		}
-	]
-};
+module.exports = require('autoroll')(require('./package.json'));
